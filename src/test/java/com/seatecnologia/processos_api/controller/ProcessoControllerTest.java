fb8e25651fa1c920715421608895bb57d300174a
Fernando <silentguyband@gmail.com>
2025-05-17 16:27:49 -0300

    Test Post

--- src/test/java/com/seatecnologia/processos_api/controller/ProcessoControllerTest.java
+++ src/test/java/com/seatecnologia/processos_api/controller/ProcessoControllerTest.java
@@ -14,9 +14,11 @@ import org.springframework.test.web.servlet.MockMvc;
 
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 import com.seatecnologia.processos_api.repository.ProcessoRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.seatecnologia.processos_api.models.Processo;
 
 @WebMvcTest(ProcessoController.class)
@@ -76,6 +78,33 @@ public class ProcessoControllerTest {
         mockMvc.perform(get("/processos/{id}", id)
                 .contentType(MediaType.APPLICATION_JSON))
             .andExpect(status().isNotFound());
+    }
+
+    @Test
+    public void testCriarProcesso_comSucesso() throws Exception {
+        Processo processoEntrada = new Processo("2024-00002", "Novo Processo", LocalDate.of(2024, 5, 17), "Aberto");
+        
+        // Simula o que o banco retorna após salvar (com ID gerado)
+        Processo processoSalvo = new Processo("2024-00002", "Novo Processo", LocalDate.of(2024, 5, 17), "Aberto");
+        processoSalvo.setId(10L);
+
+        // Quando salvar o processo, retorna o processo salvo com id
+        Mockito.when(processoRepository.save(Mockito.any(Processo.class)))
+               .thenReturn(processoSalvo);
+
+        // Converter o processoEntrada para JSON
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.registerModule(new com.fasterxml.jackson.datatype.jsr310.JavaTimeModule());
+        String json = mapper.writeValueAsString(processoEntrada);
+
+        mockMvc.perform(post("/processos")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(json))
+            .andExpect(status().isCreated())
+            .andExpect(jsonPath("$.id").value(10L))
+            .andExpect(jsonPath("$.numeroProcesso").value("2024-00002"))
+            .andExpect(jsonPath("$.assunto").value("Novo Processo"))
+            .andExpect(jsonPath("$.status").value("Aberto"));
     }
 
 }
